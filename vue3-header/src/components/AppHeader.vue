b918128b1f4b5017d95bfbd6c3d60ba3a223e271
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-15 13:12:14 +0200

    added dynamic links to header navbar

--- vue3-header/src/components/AppHeader.vue
+++ vue3-header/src/components/AppHeader.vue
@@ -2,15 +2,31 @@
     <navbar id="header-navbar">
         <h1>My Ugly Navbar</h1>
         <ul>
-            <li>Test</li>
-            <li>Test</li>
-            <li>Test</li>
+            <li v-for="link in links"><a :href="link.link">{{ link.name }}</a></li>
         </ul>
     </navbar>
 </template>
 <script>
 export default {
-    
+    name: 'AppHeader',
+    data() {
+            return {
+                links: [
+                    {
+                        name: 'Home',
+                        link: '#'
+                    },
+                    {
+                        name: 'Prodotti',
+                        link: '#'
+                    },
+                    {
+                        name: 'Contatti',
+                        link: '#'
+                    }
+                ]
+            }
+        },
 }
 </script>
 <style lang="scss" scoped>
