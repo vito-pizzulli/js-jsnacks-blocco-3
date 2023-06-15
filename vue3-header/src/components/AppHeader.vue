05b004b25840dc3e5ead73386bd7843b46d0a1a3
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-15 12:52:43 +0200

    created AppHeader.vue and added to App.vue

--- vue3-header/src/components/AppHeader.vue
+++ vue3-header/src/components/AppHeader.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-        
-    </div>
+    <navbar id="header-navbar">
+        <h1>My Ugly Navbar</h1>
+        <ul>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </ul>
+    </navbar>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
     
 </style>
